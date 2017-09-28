5f5043c5e08f30c892fa51e9cbdd77fb5bb85e33
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-09-28 13:04:56 -0400

    Adding check for 1 and 5

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
 
 // init of Global vars
 int result = 0;
@@ -6,6 +10,13 @@ int value = 0;
 
 
 int main(int argc, char *argv[]) {
-	
+	// char *name = argv;
+	double number_entered;
    
+   	cout << "Enter a number between 1 and 5: " << endl;
+   	cin >> number_entered;
+   
+   	if ((number_entered < 1) && (number_entered > 5)) {
+   		cout << "That number is not between 1 and 5." << endl;	
+   }
 }

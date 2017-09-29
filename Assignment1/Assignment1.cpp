51c69fb5fb1d424fb7eee99fb13c440779a2ecbb
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-09-29 11:47:46 -0400

    Having issue w/ iostream library...

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <iostream>
-#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
-// init of Global vars
-int result = 0;
-int value = 0;
-
 using namespace std;
 
+int result = 0;
+int value = 0;
 
 int main(int argc, char *argv[]) {
 	// char *name = argv;
 	
+   	cout << "Enter a number between 1 and 5: " << endl;
+   	cin >> value;
    
-//   	cout << "Enter a number between 1 and 5: " << endl;
-//   	cin >> value;
-//   
-//   	if ((value < 1) && (value > 5)) {
-//   		cout << "That number is not between 1 and 5." << endl;	
-//   }
-   
+   	if ((value < 1) && (value > 5)) {
+   		cout << "That number is not between 1 and 5." << endl;
+		   
+		double modulo = value % 1;
+		if (modulo != 0) {
+			cout << "The number you entered is not an integer. Please enter again" << endl;
+			cin >> value;
+		}	
+   }
 }

ff684654e98aed3c6315b2bc958efa4c7aa5d11d
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-09-29 10:58:30 -0400

    GCC Won't compile again
    Maybe It's just me

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <iostream>
-#include <cmath>
-
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 // init of Global vars
 int result = 0;
 int value = 0;
 
+using namespace std;
+
 
 int main(int argc, char *argv[]) {
 	// char *name = argv;
-	double number_entered;
+	
    
-   	cout << "Enter a number between 1 and 5: " << endl;
-   	cin >> number_entered;
+//   	cout << "Enter a number between 1 and 5: " << endl;
+//   	cin >> value;
+//   
+//   	if ((value < 1) && (value > 5)) {
+//   		cout << "That number is not between 1 and 5." << endl;	
+//   }
    
-   	if ((number_entered < 1) && (number_entered > 5)) {
-   		cout << "That number is not between 1 and 5." << endl;	
-   }
 }

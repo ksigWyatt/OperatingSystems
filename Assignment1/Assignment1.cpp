65228bf783db92896a417f42a6a23c26cbd92609
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-09-30 12:10:23 -0400

    Working better now

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -1,30 +1,26 @@
 #include <stdio.h>
 #include <iostream>
-#include <string>
-#include <sstream>
 #include <string.h>
 #include <unistd.h>
 
 using namespace std;
 
 int result = 0;
-
+int value = 0;
 
 int main(int argc, char *argv[]) {
 	// char *name = argv;
-	int value = 0;
 	
-   	cout << "Enter a number between 1 and 5: " << endl;
-   	getline(cin, value);
+   	cout << "Enter an integer between 1 and 5: " << endl;
+   	cin >> value;
    	
    
-   	if ((value < 1) && (value > 5)) {
+   	if ((value < 1) || (value > 5)) {
    		cout << "That number is not between 1 and 5." << endl;
-		   
-		   
-		if (cin.fail()) {
-			cout << "The number you entered is not a integer" << endl;
-		}
+		
+	} 
+	else if ((value % 1) != 0) {
+		cout << "The number you entered is not a integer. " << endl;
 	}
 	return 0;
 }

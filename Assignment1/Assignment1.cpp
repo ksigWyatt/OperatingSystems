d708d064f4ca387e5cf227abf6bfc147afab8bad
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-09-29 21:12:06 -0400

    g++ compiles but the if statement isn't reached
    not sure what the issuse is

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -1,26 +1,30 @@
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <string.h>
 #include <unistd.h>
 
 using namespace std;
 
 int result = 0;
-int value = 0;
+
 
 int main(int argc, char *argv[]) {
 	// char *name = argv;
+	int value = 0;
 	
    	cout << "Enter a number between 1 and 5: " << endl;
-   	cin >> value;
+   	getline(cin, value);
+   	
    
    	if ((value < 1) && (value > 5)) {
    		cout << "That number is not between 1 and 5." << endl;
 		   
-		double modulo = value % 1;
-		if (modulo != 0) {
-			cout << "The number you entered is not an integer. Please enter again" << endl;
-			cin >> value;
-		}	
-   }
+		   
+		if (cin.fail()) {
+			cout << "The number you entered is not a integer" << endl;
+		}
+	}
+	return 0;
 }

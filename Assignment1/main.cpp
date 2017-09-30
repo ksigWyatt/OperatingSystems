9c1eea60e4fabe3d1425695cd89d4b301516dcb4
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-09-30 17:29:30 -0400

    Now we're getting somewhere

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -9,15 +9,29 @@ using namespace std;
 int result = 0;
 int value = 0;
 
-int main(int argc, char** argv) {
-    cout << "Enter an integer between 1 and 5: " << endl;
-    cin >> value;
-
-
-    if ((value < 1) || (value > 5)) {
-        cout << "That number is not between 1 and 5." << endl;
-    } 
+int calculateFactorial(int num) {
     
+    result = 1;
+    return result; 
+}
+
+int main(int argc, char** argv) {
+    
+    // Create a child process
+    int child_id = fork();
+    
+    if (child_id == 0) {
+
+       printf("Child of _ is % d\n", getpid());
+       return 0;
+
+    } 
+    else if(child_id > 0){
+        
+	result = calculateFactorial(value);
+    }
+    cout << "the result is: " << result << endl;
     return 0;
 }
+
 

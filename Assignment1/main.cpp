9c3bdc06113ca0087f93cff5bcb5523bd8ea9f88
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-10-01 18:03:15 -0400

    Back at it again

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 
 using namespace std;
 
@@ -26,26 +28,29 @@ int main(int argc, char** argv) {
     // Create a child process
     int child_id = fork();
     
-    // Inside the parent
+    // Inside the child
     if (child_id == 0) {
         
         //printf("Child of % s is % d\n", name, getpid());
-        //wait(1);
-        printf("Factorial of %d = %d\n", value, result);
-    } 
-    // inside the child
-    else {
-        //printf("My id is % d\n", getpid());
         
         cout << "Enter an integer" << endl;
         cin >> value;
-        
-        
+       
         if ((value > 1) || (value < 5)) {
             // Store result of 
             result = calculateFactorial(value);
             cout << result << " is the result of the factorial. " << endl;
         }
+    } 
+    // inside the parent
+    else {
+        //printf("My id is % d\n", getpid());
+        
+        printf("Factorial of %d = %d\n", value, result);
+        
+        // Testing a theory
+        // that didn't work either - `a` must be of type something I declare
+        // int a = wait(&value);
     }
     return 0;
 }

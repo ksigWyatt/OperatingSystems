80b80a1f138361739d32daebb2e875fd3f32e85f
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-10-03 12:05:09 -0400

    Got it finally, I knew I was close

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -8,11 +8,13 @@
 
 using namespace std;
 
+// init of Global vars
 int result = 0;
 int value = 0;
 
 int calculateFactorial(int value) {
-    // n * n-1
+    // init of result to 1
+    result = 1;
     for(int i = 1; i <= value; ++i) {
         result *= i;
     }
@@ -28,36 +30,38 @@ int main(int argc, char* argv[]) {
     
     int status = 0;
     
+    // Ask for input
+    cout << "Enter an integer" << endl;
+    cin >> value;
+    
     // Create a child process
-    //char process = argv[0];
     pid_t pid = fork();
     
     // Inside the child
     if (pid == 0) {
-        
-        printf("Child pid is % d\n" ,getpid());
-        
-        cout << "Enter an integer" << endl;
-        cin >> value;
-       
+            
         if ((value > 1) || (value < 5)) {
             // Store result of 
             result = calculateFactorial(value);
             
-            // getting 1280 for the factorial?
-            exit(0);
+            // Alert the waiting processes that we are done
+            exit(result);
         }
     } 
-    // inside the parent
+    // Inside the parent
     else if (pid > 0){
-        printf("My id is % d\n\n", getpid());
         
-        // waiting for the child to finish
-        wait(&status);
+        // Waiting for the child to finish
+        wait(&result);
+        wait(&value);
+        // Fixing the result value - the value is multiplied by 255
+        result = result / 255;
         
+        // Output as specified by the assignment
         printf("Factorial of %d = %d\n", value, result);
         
     }
+    // If the fork failed handle the exception - end the program with code 1
     else {
         printf("Fork Failed!\n");
         return 1;

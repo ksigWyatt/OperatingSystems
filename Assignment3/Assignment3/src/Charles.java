afb8d87628b178110b3953f3f546a126500064fd
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-10-31 15:20:31 -0400

    Assignment 3 init - Using Java

---
+++ Assignment3/Assignment3/src/Charles.java
@@ -0,0 +1,14 @@
+
+public class Charles implements Runnable {
+
+	public Charles(Garden garden) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Assignment3/Assignment3/src/Tracy.java
@@ -0,0 +1,14 @@
+
+public class Tracy implements Runnable {
+
+	public Tracy(Garden garden) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

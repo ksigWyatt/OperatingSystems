4648d46af7f54e14d236c6f323f0b753a9b8797e
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-02 09:50:50 -0400

    Begin adding Locks & Conditions

--- Assignment3/Assignment3/src/Garden.java
+++ Assignment3/Assignment3/src/Garden.java
@@ -1,14 +1,52 @@
+import java.util.concurrent.locks.*;
+
 
 public class Garden {
-
-	public void waitToDig() {
-		// TODO Auto-generated method stub
+	// Initialize the locks
+	final Lock lock = new ReentrantLock();
+	final Condition unFull  = lock.newCondition(); 
+	final Condition full = lock.newCondition(); 
+	
+	public void waitToDig() throws InterruptedException {
 		
 	}
 
-	public void dig() {
-		// TODO Auto-generated method stub
+	// dig the hole
+	public void dig() throws InterruptedException {
+		lock.lock(); // Set lock
+		try {
+			// unFull.await();
+			// full.signal(); // signal waiting thread
+		} finally {
+			lock.unlock();
+		}
 		
+	}
+	
+	// Plant something in the hole
+	public void plant() throws InterruptedException {
+		lock.lock(); // set lock
+		
+		try {
+			// unFull.await();
+			
+			// full.signal(); // signal waiting thread
+		} finally {
+			lock.unlock();
+		}
+	}
+	
+	// Fill over the hole
+	public void fill() throws InterruptedException {
+		lock.lock(); // set lock
+		
+		try {
+			// unFull.await();
+			
+			//full.signal(); // signal waiting thread
+		} finally {
+			lock.unlock();
+		}
 	}
 
 }

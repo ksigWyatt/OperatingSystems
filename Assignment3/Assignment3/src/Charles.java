047b23514dbae9d8aebd66ed13c788f909ff257d
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-02 15:58:44 -0400

    Setting some comments & changing sleep time
    TODOs for the features that still need to get added to the application

--- Assignment3/Assignment3/src/Charles.java
+++ Assignment3/Assignment3/src/Charles.java
@@ -9,13 +9,15 @@ public class Charles implements Runnable {
 	        this.garden = g;
 	    }
 
+	    // It takes Charles less than 500 milliseconds to plant something in a hole.
+	    // Charles will wait if all holes are with plants.
 	    public void run() {
 	        try {
 	            Thread.sleep(rand.nextInt(1000)); // makes the execution more random
 	            for (int i = 0; i < 10; i++) {
 	                garden.waitToDig();
-	                garden.dig();
-	                Thread.sleep(rand.nextInt(100)); // digging
+	                garden.plant();
+	                Thread.sleep(rand.nextInt(500)); // filling
 	            }
 	        } catch (InterruptedException e) {
 	            System.out.println(e);
--- Assignment3/Assignment3/src/Tracy.java
+++ Assignment3/Assignment3/src/Tracy.java
@@ -9,13 +9,15 @@ public class Tracy implements Runnable {
 	        this.garden = g;
 	    }
 
+	    // It takes Tracy less than 500 milliseconds to fill a hole.
+	    // Tracy fills these ten holes after being planted.
 	    public void run() {
 	        try {
 	            Thread.sleep(rand.nextInt(1000)); // makes the execution more random
 	            for (int i = 0; i < 10; i++) {
 	                garden.waitToDig();
-	                garden.dig();
-	                Thread.sleep(rand.nextInt(100)); // digging
+	                garden.fill();
+	                Thread.sleep(rand.nextInt(500)); // digging
 	            }
 	        } catch (InterruptedException e) {
 	            System.out.println(e);
